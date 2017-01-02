be283c7c4187b3209f40229109aca0803f5c5f9e
James Christopher Adduono <jc@adduono.com>
2017-01-02 09:23:33 -0500

    Revert "HID keyboard/mouse gadget support"
    This reverts commit 0aa0dff55b2f1aaff3cd6c32ca877aa6536040c7.

--- include/linux/usb/android.h
+++ include/linux/usb/android.h
@@ -42,7 +42,6 @@ enum android_function_index {
 	ANDROID_AUDIO_SRC,
 	ANDROID_CHARGER,
 	ANDROID_MIDI,
-	ANDROID_HID,
 	ANDROID_RNDIS_GSI,
 	ANDROID_ECM_GSI,
 	ANDROID_RMNET_GSI,
@@ -101,8 +100,6 @@ static enum android_function_index name_to_func_idx(const char *name)
 		return ANDROID_AUDIO_SRC;
 	if (!strncasecmp("MIDI", name, FUNC_NAME_LEN))
 		return ANDROID_MIDI;
-	if (!strncasecmp("HID", name, FUNC_NAME_LEN))
-		return ANDROID_HID;
 	if (!strncasecmp("RNDIS_GSI", name, FUNC_NAME_LEN))
 		return ANDROID_RNDIS_GSI;
 	if (!strncasecmp("ECM_GSI", name, FUNC_NAME_LEN))
